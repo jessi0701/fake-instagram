49d15bd895b65394f04efceb27fcb48ea1f6d8bb
Kim Nok Hyeong <jessi0701@naver.com>
2019-04-09 02:11:58 +0000

    이미지 업로드 구현

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 # Create your models here.
 class Post(models.Model):
     content = models.CharField(max_length=100)
-    
+    #image = models.ImageField(blank=True)
+    image = ProcessedImageField(
+                upload_to='posts/images',
+                processors=[ResizeToFill(600,600)],
+                format='JPEG',
+                options={'quality':90},
+                )
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import PostForm
+from .models import Post
 
 # Create your views here.
 def list(request):
-    return render(request, 'posts/list.html')
+    posts = Post.objects.all()
+    return render(request, 'posts/list.html',{'posts':posts})
+    
+def create(request):
+    # 1. get 방식으로 데이터를 입력할 form을 요청한다.
+    # 4. 사용자가 데이터를 입력해서 post방식으로 요청한다.
+    # 9. 사용자가 다시 적절한 데이터를 입력해서 post방식으로 요청한다.
+    if request.method == "POST":
+        # 5. post방식으로 저장요청을 받고, 데이터를 받아 PostForm에 넣어서 인스턴스화 한다.
+        # 10. 5번과 같음
+        form = PostForm(request.POST,request.FILES)
+        # 6. 데이터 검증을 한다.
+        # 11. 6번과 같음
+        if form.is_valid():
+            # 12. 적절한 데이터가 들어온다. 데이터를 저장하고 list페이지로 리다이렉트!!
+            form.save()
+            return redirect("posts:list")
+        else:
+            # 7. 적절하지 않은 데이터가 들어온다.
+            pass
+            
+    else:
+        # 2. PostForm을 인스턴스화 시켜서 form에 저장한다.
+        form = PostForm()
+    # 3. form 을 담아서 create.html을 보내준다.
+    # 8. 사용자가 입력한 데이터는 form에 담아진 상태로 다시 form을 담아서 create.html을 보내준다.
+    return render(request, 'posts/form.html', {'form':form})
+        
+def update(request,id):
+    post = Post.objects.get(id=id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'posts/form.html',{'form':form})
+    
+def delete(request,id):
+    post = Post.objects.get(id=id)
+    post.delete()
+    
+    return redirect("posts:list")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
